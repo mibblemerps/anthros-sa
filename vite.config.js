e7489387ea4ec00be2edb25e569e04404d807577
Mibble <mibble@outlook.com.au>
2026-07-02 20:51:08 +0930

    Fix vite

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
+import * as fs from 'fs';
+import * as path from 'path';
+
+// Read all scss files from the target directory
+const scssDir = 'resources/css';
+const scssFiles = fs.readdirSync(scssDir)
+    .filter(file => file.endsWith('.scss'))
+    .map(file => path.join(scssDir, file).replace(/\\/g, '/'));
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/*.scss', 'resources/js/app.js', 'resources/js/home.js', 'resources/js/upload.js'],
+            input: [...scssFiles, 'resources/js/app.js', 'resources/js/home.js', 'resources/js/upload.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
