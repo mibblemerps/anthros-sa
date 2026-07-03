441ebbb2613bfa0136fc03eae22eb4d9e2094cf6
Mibble <mibble@outlook.com.au>
2026-07-03 21:42:27 +0930

    More carousel tweaks

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,17 +1,22 @@
 import EmblaCarousel from 'embla-carousel'
 import AutoScroll from "embla-carousel-auto-scroll";
 
+let reverse = false;
+
 function setupCarousel(el) {
     const viewportNode = el.querySelector('.embla__viewport');
 
     const emblaApi = EmblaCarousel(viewportNode, { loop: true }, [AutoScroll({
         startDelay: 0,
-        speed: 1.5,
+        speed: 0.5 * (reverse ? 1 : -1),
         stopOnMouseEnter: true,
         stopOnInteraction: false,
     })]);
 
     emblaApi.plugins().autoplay?.play()
+
+    // reverse the next carousel
+    reverse = !reverse;
 }
 
 
