604c528c902c900dfdb92b0dd539cd55aab68811
Mibble <mibble@outlook.com.au>
2026-07-05 01:13:12 +0930

    Add photographer credit to slideshow on homepage

--- resources/views/home/_slides.blade.php
+++ resources/views/home/_slides.blade.php
@@ -2,7 +2,12 @@
     <div class="embla__viewport">
         <div class="embla__container">
             @foreach ($photos as $photo)
-                <div class="embla__slide"><a href="/photo/{{ $photo->id }}"><img src="{{ $photo->thumbnail_file }}" alt="{{ $photo->title ?? 'Photo' }}"></a></div>
+                <div class="embla__slide">
+                    <a href="/photo/{{ $photo->id }}">
+                        <img src="{{ $photo->thumbnail_file }}" alt="{{ $photo->title ?? 'Photo' }}">
+                    </a>
+                    <div class="photo-credit"><i class="fa fa-camera"></i> {{ $photo->user->name }}</div>
+                </div>
             @endforeach
         </div>
     </div>
