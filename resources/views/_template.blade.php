602f2b05a675626e6bd0ddaa3e0cca35beea16a4
Mibble <mibble@outlook.com.au>
2026-07-07 21:53:05 +0930

    Add email to homepage

--- resources/views/_template.blade.php
+++ resources/views/_template.blade.php
@@ -21,16 +21,17 @@
     <a class="instagram" href="https://www.instagram.com/anthrosocietysa/"><i class="fa-brands fa-instagram"></i> @anthrosocietysa</a>
     <a class="twitter" href="https://x.com/Anthro_SA"><i class="fa-brands fa-twitter"></i> @Anthro_SA</a>
     <!--<a class="tiktok" href="https://www.tiktok.com/@anthros.sa"><i class="fa-brands fa-tiktok"></i> @anthros.sa</a>-->
+    <a class="email" href="mailto:staff@anthros.com.au"><i class="fa fa-envelope"></i> staff@anthros.com.au</a>
 </div>
 
 @yield('body')
 
 <footer id="main-footer">
     <p>Site made with &hearts; by Mibble | Banner photo by Donut</p>
+    <p>Contact us: <a href="mailto:staff@anthros.com.au">staff@anthros.com.au</a></p>
     @if (auth()->user() !== null)
         <p>Logged in as <b>{{ auth()->user()->name }}</b>. <a href="/logout">Logout</a></p>
     @endif
-
 </footer>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
